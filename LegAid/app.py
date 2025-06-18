25b568dbc5a7bd7050810a2cd0cf8b9932e0d612
clcriswell <codycriswell@yahoo.com>
2025-06-18 07:51:52 -0700

    Update app.py

--- LegAid/app.py
+++ LegAid/app.py
@@ -2,13 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="LegAid", page_icon="📜", layout="wide")
 
-# Display logo image (must be a valid path!)
-st.image("Assets/MainLogo.png", width=150)
-
-# Now display the HTML-styled title and subtitle
 st.markdown(
     """
-    <div style='text-align:center; padding:0 0 2rem 0;'>
+    <div style='text-align:center; padding:2rem 0;'>
+        <img src='./Assets/MainLogo.png' width='150' alt='LegAid logo'>
         <h1 style='margin-bottom:0;'>LegAid</h1>
         <h4 style='margin-top:0; color: gray;'>Tools for Legislative Productivity</h4>
     </div>
@@ -17,4 +14,3 @@ st.markdown(
 )
 
 st.write("Select a tool from the sidebar to begin.")
-
