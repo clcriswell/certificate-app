4f5924b352926d5ff2c046dfb95659955f61db1b
clcriswell <codycriswell@yahoo.com>
2025-06-18 07:47:29 -0700

    Update app.py

--- LegAid/app.py
+++ LegAid/app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="LegAid", page_icon="📜", layout="wide")
 st.markdown(
     """
     <div style='text-align:center; padding:2rem 0;'>
-        <img src='LegAid/Assets/MainLogo.png' width='150' alt='LegAid logo'>
+        st.image("Assets/MainLogo.png", width=150)
         <h1 style='margin-bottom:0;'>LegAid</h1>
         <h4 style='margin-top:0; color: gray;'>Tools for Legislative Productivity</h4>
     </div>
