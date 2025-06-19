dc0913bade0d36de9104f95b695d21b5ff197619
clcriswell <codycriswell@yahoo.com>
2025-06-19 10:17:24 -0700

    Update 1_CertCreate.py

--- LegAid/pages/1_CertCreate.py
+++ LegAid/pages/1_CertCreate.py
@@ -1344,7 +1344,7 @@ def generate_pdf_certificates(entries):
         if title_provided:
             text_start_y = y
             c.setFont("Times-Roman", text_size)
-            y = text_start_y - 0.25 * inch
+            y = text_start_y - 0.0 * inch
         for line in wrap_text(entry["Certificate_Text"], "Times-Roman", text_size, avail_width):
             c.drawCentredString(center_x, y, line)
             y -= text_size * 1.2
