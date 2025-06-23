7138b9f31f45a33cce24932b10616d317c2cd320
clcriswell <codycriswell@yahoo.com>
2025-06-23 15:07:35 -0700

    Update app.py

--- LegAid/app.py
+++ LegAid/app.py
@@ -56,9 +56,9 @@ st.markdown("""
 # Place buttons directly below the logo/title block
 st.markdown("""
 <div class='centered-btn-container'>
-    <a href='/pages/1_CertCreate'><button>CertCreate</button></a>
-    <a href='/2_SpeechCreate'><button>SpeechCreate</button></a>
-    <a href='/3_ResponseCreate'><button>ResponseCreate</button></a>
+    <a href='CertCreate'><button>Certificates</button></a>
+    <a href='SpeechCreate'><button>Speeches</button></a>
+    <a href='ResponseCreate'><button>Respond</button></a>
 </div>
 """, unsafe_allow_html=True)
 
