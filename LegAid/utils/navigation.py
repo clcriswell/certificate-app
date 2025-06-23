ca137be49c9854e4bbd3f24d6b849de6e07e7e6b
clcriswell <codycriswell@yahoo.com>
2025-06-23 13:44:22 -0700

    Adjust logo spacing

--- LegAid/utils/navigation.py
+++ LegAid/utils/navigation.py
@@ -74,8 +74,8 @@ def render_logo():
         <style>
         .logo-container {{
             position: fixed;
-            top: 20px;
-            right: 20px;
+            top: 50px;
+            right: 60px;
             z-index: 999;
         }}
 
@@ -88,8 +88,8 @@ def render_logo():
                 width: 120px; /* reduce logo size on smaller screens */
             }}
             .logo-container {{
-                top: 10px;
-                right: 10px;
+                top: 20px;
+                right: 20px;
             }}
         }}
         </style>
