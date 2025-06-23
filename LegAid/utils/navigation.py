a4e08c30e0b47d61de2b9472e80a7b762e037c1a
clcriswell <codycriswell@yahoo.com>
2025-06-23 11:14:15 -0700

    Update navigation.py

--- LegAid/utils/navigation.py
+++ LegAid/utils/navigation.py
@@ -49,7 +49,7 @@ def render_sidebar():
     with st.sidebar:
         st.page_link("app.py", label="LegAid", icon=None)
 
-        st.page_link("pages/1_CreateCreate.py", label="CearteCreate", icon=None)
+        st.page_link("pages/1_CertCreate.py", label="CertCreate", icon=None)
             
         st.page_link("pages/2_SpeechCreate.py", label="SpeechCreate", icon=None)
 
